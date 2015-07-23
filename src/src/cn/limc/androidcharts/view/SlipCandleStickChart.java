3f56ccdbab04d1f4f18e180eff7adfce9b89e944
limccn <limc.cn@me.com>
2015-07-23 17:28:50 +0800

    Source Refactoring
    Source Refactoring

---
+++ src/src/cn/limc/androidcharts/view/SlipCandleStickChart.java
@@ -0,0 +1,145 @@
+/*
+ * SlipCandleStickChart.java
+ * Android-Charts
+ *
+ * Created by limc on 2014.
+ *
+ * Copyright 2011 limc.cn All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.limc.androidcharts.view;
+
+import cn.limc.androidcharts.entity.IMeasurable;
+import cn.limc.androidcharts.mole.CandleStickMole;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+
+/**
+ * <p>
+ * en
+ * </p>
+ * <p>
+ * jp
+ * </p>
+ * <p>
+ * cn
+ * </p>
+ * 
+ * @author limc
+ * @version v1.0 2014/01/21 11:37:51
+ * 
+ */
+public class SlipCandleStickChart extends SlipStickChart {
+
+	/**
+	 * <p>
+	 * Constructor of SlipCandleStickChart
+	 * </p>
+	 * <p>
+	 * SlipCandleStickChart类对象的构造函数
+	 * </p>
+	 * <p>
+	 * SlipCandleStickChartのコンストラクター
+	 * </p>
+	 * 
+	 * @param context
+	 * @param attrs
+	 * @param defStyle
+	 */
+	public SlipCandleStickChart(Context context, AttributeSet attrs,
+			int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * <p>
+	 * Constructor of SlipCandleStickChart
+	 * </p>
+	 * <p>
+	 * SlipCandleStickChart类对象的构造函数
+	 * </p>
+	 * <p>
+	 * SlipCandleStickChartのコンストラクター
+	 * </p>
+	 * 
+	 * @param context
+	 * @param attrs
+	 */
+	public SlipCandleStickChart(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * <p>
+	 * Constructor of SlipCandleStickChart
+	 * </p>
+	 * <p>
+	 * SlipCandleStickChart类对象的构造函数
+	 * </p>
+	 * <p>
+	 * SlipCandleStickChartのコンストラクター
+	 * </p>
+	 * 
+	 * @param context
+	 */
+	public SlipCandleStickChart(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * <p>Called when is going to draw this chart<p> <p>チャートを書く前、メソッドを呼ぶ<p>
+	 * <p>绘制图表时调用<p>
+	 * 
+	 * @param canvas
+	 * 
+	 * @see android.view.View#onDraw(android.graphics.Canvas)
+	 */
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+	}
+
+	
+	   @Override
+	    protected void drawSticks(Canvas canvas) {
+	        if (null == stickData) {
+	            return;
+	        }
+	        if (stickData.size() == 0) {
+	            return;
+	        }
+
+	        float stickWidth = dataQuadrant.getPaddingWidth() / getDisplayNumber();
+	        float stickX = dataQuadrant.getPaddingStartX();
+
+	        for (int i = getDisplayFrom(); i < getDisplayTo(); i++) {
+	            IMeasurable stick = stickData.get(i);
+	            
+	            CandleStickMole mole = new CandleStickMole();
+	            mole.setUp(this,stick,stickX,stickWidth);
+	            mole.draw(canvas);
+
+	            // next x
+	            stickX = stickX + stickWidth;
+	        }
+	    }
+}
