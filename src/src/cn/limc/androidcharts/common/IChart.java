544a56ec2dd1c0cfb53fc37ae4eca2733ddc4655
limccn <limc.cn@me.com>
2015-07-30 09:33:19 +0800

    source refacting
    source refacting

---
+++ src/src/cn/limc/androidcharts/common/IChart.java
@@ -0,0 +1,26 @@
+//
+// IChart.java
+// cn.limc.androidcharts.common
+//
+// Created by limc on 2015-7-30.
+//
+// Copyright 2015 Shanghai Okasan-Huada Computer System Co. Ltd., All rights reserved.
+//
+package cn.limc.androidcharts.common;
+
+/**
+ * IChart
+ * Description: <br>
+ *   <p>add description here </p>
+ * Tags: <br>
+ *   <p> </p>
+ *
+ * @author limc
+ * @version v1.0 
+ * 
+ * History: <br>
+ * 2015-7-30 limc create v1.0 <br>
+ *
+ */
+public interface IChart {
+}
